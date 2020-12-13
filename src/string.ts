c1f736d371962b5ef9ce6d8f3e9ea6d117a5f24b
Jeremy Weinstein <jeremydw@gmail.com>
2020-12-12 23:55:38 -0800

    Add comment

--- src/string.ts
+++ src/string.ts
@@ -1,5 +1,8 @@
 import {Pod} from './pod';
 
+// TODO: Add support for more complex informatoion such as holding the context,
+// the URLs of the pages where the string is used, forcing a newer version of a
+// string to use for multi-level translation selection, etc.
 export class TranslationString {
   pod: Pod;
   value: string;

304aefb9d332ac707a3d450219b7f724a4754843
Jeremy Weinstein <jeremydw@gmail.com>
2020-12-12 23:50:45 -0800

    Collect empty strings at page build time and add them to the caches

--- src/locale.ts
+++ src/locale.ts
@@ -1,14 +1,17 @@
 import {Pod} from './pod';
+import {TranslationString} from './string';
 
 export class Locale {
   pod: Pod;
   podPath: string;
   id: string;
+  emptyStrings: Set<TranslationString>;
 
   constructor(pod: Pod, id: string) {
     this.pod = pod;
     this.id = id;
     this.podPath = `/locales/${id}.yaml`;
+    this.emptyStrings = new Set();
   }
 
   toString() {
@@ -21,8 +24,14 @@ export class Locale {
 
   getTranslation(value: string) {
     if (!this.pod.fileExists(this.podPath) || !this.translations) {
+      this.emptyStrings.add(new TranslationString(this.pod, value));
       return value;
     }
-    return this.translations[value] || value;
+    const foundValue = this.translations[value];
+    if (foundValue) {
+      return foundValue;
+    }
+    this.emptyStrings.add(new TranslationString(this.pod, value));
+    return value;
   }
 }

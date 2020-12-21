5dae5199aba63563acb981eeceea3643335a07a1
Jeremy Weinstein <jeremydw@gmail.com>
2020-12-20 21:23:23 -0800

    Add initial TypeDoc-generated site (#16)
    * Add initial typedoc integration
    
    * Make all properties of the pod readonly
    
    * Add a few more docs

--- src/pod.ts
+++ src/pod.ts
@@ -14,14 +14,20 @@ import {StaticFile} from './static';
 import {getRenderer} from './renderer';
 import {join} from 'path';
 
+/**
+ * Pods are the "command center" for all operations within a site. Pods hold
+ * references to things like the build environment, template engines, file
+ * system accessors, routes, etc. Pods provide an interaction model for accessing
+ * the different elements of a site and operating on them.
+ */
 export class Pod {
   static DefaultLocale = 'en';
-  builder: Builder;
-  cache: Cache;
-  env: Environment;
+  readonly builder: Builder;
+  readonly cache: Cache;
+  readonly env: Environment;
   readonly profiler: Profiler;
-  root: string;
-  router: Router;
+  readonly root: string;
+  readonly router: Router;
 
   constructor(root: string) {
     // Anything that occurs in the Pod constructor must be very lightweight.
@@ -39,6 +45,13 @@ export class Pod {
     this.cache = new Cache(this);
   }
 
+  /**
+   * Returns a collection object. If no `_collection.yaml` file is found within
+   * the requested directory, the directory will be walked upwards until finding
+   * a directory containing a `_collection.yaml` file. If no `_collection.yaml`
+   * is found, no collection will be returned.
+   * @param path The podPath to the collection.
+   */
   collection(path: string) {
     if (this.cache.collections[path]) {
       return this.cache.collections[path];
@@ -51,10 +64,20 @@ export class Pod {
     return this.cache.collections[path];
   }
 
+  /**
+   * Returns the default locale for the pod. The default locale can be
+   * overwritten in `amagaki.yaml`.
+   */
   get defaultLocale() {
     return this.locale(Pod.DefaultLocale);
   }
 
+  /**
+   * Returns a document object.
+   * @param path The podPath to the document.
+   * @param locale The document's locale. If not provided, the pod's default
+   * locale will be used to return the document.
+   */
   doc(path: string, locale?: Locale) {
     locale = locale || this.defaultLocale;
     const key = `${path}${locale.id}`;
@@ -65,6 +88,10 @@ export class Pod {
     return this.cache.docs[key];
   }
 
+  /**
+   * Returns whether a file exists within the pod.
+   * @param path The podPath to the file.
+   */
   fileExists(path: string) {
     const timer = this.profiler.timer('file.exists', 'File exists');
     try {
@@ -74,11 +101,19 @@ export class Pod {
     }
   }
 
+  /**
+   * Returns the absolute file path on the file system.
+   * @param path The podPath to the file or directory.
+   */
   getAbsoluteFilePath(path: string) {
     path = path.replace(/^\/+/, '');
     return join(this.root, path);
   }
 
+  /**
+   * Returns a locale object.
+   * @param id The locale identifier code.
+   */
   locale(id: string) {
     if (this.cache.locales[id]) {
       return this.cache.locales[id];
@@ -87,6 +122,10 @@ export class Pod {
     return this.cache.locales[id];
   }
 
+  /**
+   * Returns a set of the pod's global locales. Global locales are defined in
+   * `amagaki.yaml`.
+   */
   get locales(): Set<Locale> {
     // TODO: Replace with amagaki.yaml?locales.
     return new LocaleSet();
@@ -135,11 +174,19 @@ export class Pod {
     return this.cache.yamlStrings[cacheKey];
   }
 
+  /**
+   * Returns a template renderer used for a template engine.
+   * @param path The renderer's file extension, without a leading dot. Example: "njk".
+   */
   renderer(path: string) {
     const rendererClass = getRenderer(path);
     return new rendererClass(this);
   }
 
+  /**
+   * Returns a static file object.
+   * @param path The podPath to the static file.
+   */
   staticFile(path: string) {
     if (this.cache.staticFiles[path]) {
       return this.cache.staticFiles[path];
@@ -148,6 +195,10 @@ export class Pod {
     return this.cache.staticFiles[path];
   }
 
+  /**
+   * Returns a translation string object.
+   * @param options The options of the translation string.
+   */
   string(options: StringOptions) {
     if (this.cache.strings[options.value]) {
       return this.cache.strings[options.value];
@@ -156,10 +207,19 @@ export class Pod {
     return this.cache.strings[options.value];
   }
 
+  /**
+   * Walks a podPath directory an returns a flattened list of podPaths within
+   * that directory, recursively in a flat list. For example, if walking
+   * `/content/pages/`, it might return: `/content/pages/index.yaml`,
+   * `/content/pages/subpages/index.yaml`, etc. */
   walk(path: string) {
     return utils.walk(this.getAbsoluteFilePath(path), [], this.root);
   }
 
+  /**
+   * Returns the YAML schema used to serialize and deserialize all YAML
+   * documents that go through the pod.
+   */
   get yamlSchema() {
     if (this.cache.yamlSchema) {
       return this.cache.yamlSchema;

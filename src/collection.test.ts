d2b7ef9d553695bcf8b270a5415cbc2e10b9a38c
Randy Merrill <randy@blinkk.com>
2021-03-29 11:25:14 -0600

    Move text fixtures into separate directory.
    Allows for making fixtures that are more specific to a certain feature without complicating other tests.

--- src/collection.test.ts
+++ src/collection.test.ts
@@ -4,24 +4,60 @@ import {Pod} from './pod';
 import test from 'ava';
 
 test('Collection docs', (t: ExecutionContext) => {
-  const pod = new Pod('./example/');
-  const collection = pod.collection('/content/pages/') as Collection;
-  const docs = collection.docs();
-  const paths = docs.map(doc => doc.path);
+  const pod = new Pod('./fixtures/collections/');
+  let collection = pod.collection('/content/collection/') as Collection;
+  let docs = collection.docs();
+  let paths = docs.map(doc => doc.path);
 
   t.deepEqual(paths, [
-    '/content/pages/about.yaml',
-    '/content/pages/bio.md',
-    '/content/pages/contact.yaml',
-    '/content/pages/index.yaml',
-    '/content/pages/routes.yaml',
-    '/content/pages/sub-pages/index.yaml',
-    '/content/pages/template.njk',
+    '/content/collection/collection-a/collection-b/index.yaml',
+    '/content/collection/collection-a/dir/index.yaml',
+    '/content/collection/collection-a/index.yaml',
+    '/content/collection/dir/collection-c/index.yaml',
+    '/content/collection/dir/index.yaml',
+    '/content/collection/index.yaml',
+  ]);
+
+  // Sub collection is a sub-set of parent collection.
+  collection = pod.collection('/content/collection/collection-a') as Collection;
+  docs = collection.docs();
+  paths = docs.map(doc => doc.path);
+
+  t.deepEqual(paths, [
+    '/content/collection/collection-a/collection-b/index.yaml',
+    '/content/collection/collection-a/dir/index.yaml',
+    '/content/collection/collection-a/index.yaml',
   ]);
 });
 
 test('Collection parents', (t: ExecutionContext) => {
-  const pod = new Pod('./example/');
-  const collection = pod.collection('/content/pages/sub-pages/') as Collection;
-  t.deepEqual([pod.collection('/content/pages')], collection.parents);
+  // No parents.
+  const pod = new Pod('./fixtures/collections/');
+  let collection = pod.collection('/content/collection/') as Collection;
+  t.deepEqual([], collection.parents);
+
+  // Single layer of parents.
+  collection = pod.collection(
+    '/content/collection/collection-a/'
+  ) as Collection;
+  t.deepEqual([pod.collection('/content/collection/')], collection.parents);
+
+  // Multiple layers of parents.
+  collection = pod.collection(
+    '/content/collection/collection-a/collection-b/'
+  ) as Collection;
+  t.deepEqual(
+    [
+      pod.collection('/content/collection/collection-a/'),
+      pod.collection('/content/collection/'),
+    ],
+    collection.parents
+  );
+
+  // Single layer of parents withing a sub-directory.
+  // Note: Sub directory is not a valid 'parent' and should not show up.
+  collection = pod.collection(
+    '/content/collection/dir/collection-c/'
+  ) as Collection;
+  t.deepEqual([pod.collection('/content/collection/')], collection.parents);
 });
--- src/pod.test.ts
+++ src/pod.test.ts
@@ -3,13 +3,14 @@ import {Pod} from './pod';
 import test from 'ava';
 
 test('Pod collection', (t: ExecutionContext) => {
-  const pod = new Pod('./example/');
+  const pod = new Pod('./fixtures/simple/');
   const collection = pod.collection('/content/pages/');
   t.truthy(collection);
 });
 
 test('Pod docs', (t: ExecutionContext) => {
-  const pod = new Pod('./example/');
+  const pod = new Pod('./fixtures/simple/');
+
   // By default, return all docs in the pod.
   t.deepEqual(
     pod.docs().map(doc => doc.path),
@@ -17,15 +18,16 @@ test('Pod docs', (t: ExecutionContext) => {
       '/content/pages/about.yaml',
       '/content/pages/bio.md',
       '/content/pages/contact.yaml',
+      '/content/pages/dir/index.yaml',
       '/content/pages/index.yaml',
       '/content/pages/routes.yaml',
-      '/content/pages/sub-pages/index.yaml',
       '/content/pages/template.njk',
       '/content/partials/base.yaml',
       '/content/partials/footer.yaml',
       '/content/partials/header.yaml',
     ]
   );
+
   // Both strings and lists are supported.
   t.deepEqual(
     pod.docs('**/*.md').map(doc => doc.path),
@@ -37,28 +39,31 @@ test('Pod docs', (t: ExecutionContext) => {
   );
   t.deepEqual(
     pod.docs(['**/index.yaml']).map(doc => doc.path),
-    ['/content/pages/index.yaml', '/content/pages/sub-pages/index.yaml']
+    ['/content/pages/dir/index.yaml', '/content/pages/index.yaml']
   );
   t.deepEqual(
     pod.docs(['**/about.yaml', '**/index.yaml']).map(doc => doc.path),
     [
       '/content/pages/about.yaml',
+      '/content/pages/dir/index.yaml',
       '/content/pages/index.yaml',
-      '/content/pages/sub-pages/index.yaml',
     ]
   );
+
   // Default behavior requesting docs from a specific collection.
   t.deepEqual(
-    pod.docs('/content/pages/*').map(doc => doc.path),
+    pod.docs('/content/pages/**').map(doc => doc.path),
     [
       '/content/pages/about.yaml',
       '/content/pages/bio.md',
       '/content/pages/contact.yaml',
+      '/content/pages/dir/index.yaml',
       '/content/pages/index.yaml',
       '/content/pages/routes.yaml',
       '/content/pages/template.njk',
     ]
   );
+
   // Exclusively use glob syntax; missing * will result in an empty list.
   t.deepEqual(
     pod.docs('/content/pages/').map(doc => doc.path),

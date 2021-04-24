a2e79f49bfb3e63eb6841ee902888b6073f5db42
Jeremy Weinstein <jeremydw@gmail.com>
2021-04-24 11:19:51 -0700

    Update test

--- src/environment.test.ts
+++ src/environment.test.ts
@@ -6,7 +6,7 @@ import test from 'ava';
 test('Environment fields', async (t: ExecutionContext) => {
   const pod = new Pod('./fixtures/environment/');
   const route = pod.router.resolve('/') as Route;
-  t.is('default,,https://localhost/', await route.build());
+  t.is('default,,http://localhost/', await route.build());
   pod.setEnvironment('prod');
   t.is('prod,foo,https://example.com/', await route.build());
   pod.setEnvironment('staging');

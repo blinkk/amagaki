f5883e1fdec8bcfbcb9c075f1c555c4029491781
Jeremy Weinstein <jeremydw@gmail.com>
2021-07-03 13:26:25 -0700

    test: add build test

---
+++ src/commands/build.test.ts
@@ -0,0 +1,23 @@
+import {BuildCommand} from './build';
+import {ExecutionContext} from 'ava';
+import path from 'path';
+import test from 'ava';
+
+const root = path.join(__dirname, '..', '..', 'fixtures', 'simple');
+
+test('Build simple fixture', async (t: ExecutionContext) => {
+  const cmd = new BuildCommand({}, {});
+  await cmd.run(root);
+  t.pass();
+});
+
+test('Build simple fixture with patterns', async (t: ExecutionContext) => {
+  const cmd = new BuildCommand(
+    {},
+    {
+      patterns: ['**/index.yaml'],
+    }
+  );
+  await cmd.run(root);
+  t.pass();
+});

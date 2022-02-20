04a1e10d23f33b9d51da6c65816e25f5dfc7709c
Jeremy Weinstein <jeremydw@gmail.com>
2022-02-19 20:46:50 -0800

    feat: add amagaki-engine-preact package

---
+++ packages/amagaki-engine-preact/src/index.test.ts
@@ -0,0 +1,10 @@
+import {ExecutionContext} from 'ava';
+import {Pod} from '@amagaki/amagaki';
+import test from 'ava';
+
+test('Build example', async (t: ExecutionContext) => {
+  const pod = new Pod('./example/');
+  const result = await pod.builder.export();
+  t.deepEqual(result.metrics.numDocumentRoutes, 1);
+  t.assert(pod.readFile('/build/page/index.html').includes('Hello World'));
+});
